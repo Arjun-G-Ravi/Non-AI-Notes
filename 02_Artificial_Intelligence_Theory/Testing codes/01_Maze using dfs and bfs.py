3d9668c9c2ba83a2b7cacf04a38342c3a58d32cf
Arjun <arjungravi1@gmail.com>
2023-11-12 22:45:11 +0530

    Commited

--- 02_Artificial_Intelligence_Theory/Testing codes/01_Maze using dfs and bfs.py
+++ 02_Artificial_Intelligence_Theory/Testing codes/01_Maze using dfs and bfs.py
@@ -1,16 +1,18 @@
-init_state= [
+env= [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, 0, 0, 0, 1, 0, 0, 0, 0, 1],
     [1, 0, 1, 0, 0, 0, 1, 1, 0, 1],
-    [1, 0, 1, 1, 1, 0, 1, 0, 'E', 1],
+    [1, 0, 1, 1, 1, 0, 1, 0, 0, 1],
     [1, 0, 0, 0, 1, 0, 0, 0, 1, 1],
     [1, 1, 1, 0, 1, 1, 1, 1, 1, 1],
-    [1, 'S', 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ]  # 1 is wall
 
 start = (6,1)
 end = (3, 8)
+
+# Here state will the position of the object, moving around the maze
 
 class Node:
     def __init__(self, state):
@@ -18,15 +20,19 @@ class Node:
         self.parent_state = None
         self.prev_action = None
 
-
+init_state = start
 init_node = Node(init_state)
 frontier = [init_node]
 visited = []
 
-def action(state):
+def action(state):  # possible actions given a state
     pass
 
 def transition_model(state, action):
     return state # but cahnge
+
 def dfs():
     pass
+
+
+# DO LATER ... 
