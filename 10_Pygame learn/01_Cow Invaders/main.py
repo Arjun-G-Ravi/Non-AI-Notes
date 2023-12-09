7de12f91c2114deec7aba77141cebcbe7706f92d
Arjun <arjungravi1@gmail.com>
2023-12-09 21:21:27 +0530

    Works perfectly with sound and cow kills. Prolly need to add grass as cow boosts

--- 10_Pygame learn/01_Cow Invaders/main.py
+++ 10_Pygame learn/01_Cow Invaders/main.py
@@ -7,7 +7,7 @@ pygame.init()
 
 # Screen
 screen = pygame.display.set_mode((800,600))
-pygame.display.set_caption("Cow invadors")
+pygame.display.set_caption("Cow invaders")
 running = True
 # pygame.display.set_icon(pygame.image.load('10_Pygame learn/01_Space Invaders /spaceship.png')) # wont wprk in ubuntu
 
@@ -56,7 +56,7 @@ def createEnemy(ch): # img, pos, horiz_motion, vertical motion
     global num_enemy
     num_enemy += 1
 
-    monster_type = {1:['monster1.png', [random.randint(100, 700), random.randint(0, 200)], random.randint(1,5), .5],
+    monster_type = {1:['monster1.png', [random.randint(100, 700), random.randint(0, 200)], random.randint(1,5), 1],
                     2:['monster2.png', [random.randint(100, 700), random.randint(0, 50)], random.randint(5,15), .5],
                     3:['monster3.png', [random.randint(100, 700), random.randint(0, 50)], random.randint(1,5), 2] }
     enemyImg.append(pygame.image.load(monster_type[ch][0]))
@@ -78,9 +78,9 @@ def shoot_milk(milkPos):
     
 
 # Collision detection
-def isCollision(pos1, pos2):
+def isCollision(pos1, pos2, distance=27):
     dis = math.sqrt((pos1[0]-pos2[0])**2 + (pos1[1]-pos2[1])**2)
-    if dis < 27:
+    if dis < distance:
         return True
     return False
 
@@ -88,7 +88,7 @@ def isCollision(pos1, pos2):
 while running:
     
     # fill screen with grass image
-    screen.blit(pygame.image.load('grass.png'),(0,0))
+    screen.blit(pygame.image.load('grass_background.png'),(0,0))
     show_score()
  
     for event in pygame.event.get():                
@@ -148,7 +148,7 @@ while running:
             milkPos[1] = 500
             score += 1
             rand = random.randint(1,500)
-            if rand <= 50: # 5%
+            if rand <= 50: # 10%
                 createEnemy(1)
             elif rand <= 55: # 1%
                 createEnemy(2)
@@ -160,14 +160,45 @@ while running:
             milk.play()
 
         if enemyPos[e][1] > 550:
-            print("You lose")
             milk = pygame.mixer.Sound('evil_laugh.mp3')
             milk.play()
+            screen.blit(pygame.image.load('grass_background.png'),(0,0))
             screen.blit(pygame.image.load('spooky.png'), (enemyPos[e][0]-20, enemyPos[e][1]-20))
-            pygame.display.update() 
-            time.sleep(5)
-            quit(0)
-     
+            while True:
+                endFont1 = pygame.font.Font('freesansbold.ttf', 80)
+                endFont2 = pygame.font.Font('freesansbold.ttf', 60)
+                endFont3 = pygame.font.Font('freesansbold.ttf', 30)
+                gameOver = endFont1.render("GAME OVER", True, (255,255,255))
+                finalScore = endFont2.render("TOTAL SCORE: "+str(score), True, (255,255,255))
+                press = endFont3.render("Press any key to quit.", True, (255,255,255))
+                screen.blit(gameOver, (150,250))
+                screen.blit(finalScore, (150,100))
+                screen.blit(press, (240,350))
+                pygame.display.update() 
+                time.sleep(3)
+                for event in pygame.event.get():  
+                    if event.type == pygame.KEYDOWN:
+                        quit(0)
+
+        if isCollision(player_pos, enemyPos[e], 40): # Cow kills
+            screen.blit(pygame.image.load('explode.png'), enemyPos[e])
+            enemyPos[e] = [random.randint(100, 700), random.randint(0, 100)]
+            score += 1
+            rand = random.randint(1,500)
+            
+            if rand <= 100: # 20%
+                createEnemy(1)
+            elif rand <= 110: # 2%
+                createEnemy(2)
+            elif rand <= 112: # 0.4%
+                createEnemy(3)
+            else:
+                pass
+            milk = pygame.mixer.Sound('grunt.mp3')
+            milk.play()
+            milk = pygame.mixer.Sound('moo.wav')
+            milk.play()
+                         
     # place player(cow) at player_pos
     player(player_pos[0], player_pos[1])
 
@@ -181,17 +212,7 @@ while running:
             fire = False
             milkPos[1] = 500
 
-
-    if isCollision(player_pos, enemypos):
-        print(score)
-        print("GAME OVER")
-        screen.blit(pygame.image.load('spooky.png'), (enemyPos[e][0]-20, enemyPos[e][1]-20))
-        pygame.display.update() 
-        milk = pygame.mixer.Sound('evil_laugh.mp3')
-        milk.play()
-        time.sleep(5)
-        
-        break
+# 
     
     # This is where the screen gets updated with the above changes
     pygame.display.update()
