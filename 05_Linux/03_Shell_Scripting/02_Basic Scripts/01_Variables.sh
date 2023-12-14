006ea028e4d7de3939cd576c3aaee4811ddc7101
Arjun <arjungravi1@gmail.com>
2023-12-14 09:34:35 +0530

    Commited

---
+++ 05_Linux/03_Shell_Scripting/02_Basic Scripts/01_Variables.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+COW="I am a cow"
+# Variable name in all caps. No spaces before of after =
+echo "Introduce yourself cow. $COW"
+# Use dollar sign to represent variables. You can also use it with {}
+echo "${COW}, who gives you milk"
+
