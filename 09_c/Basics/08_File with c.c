31396caf4c34112f5946b85457d2337ac459f9bc
Arjun <arjungravi1@gmail.com>
2023-12-15 20:39:54 +0530

    Commited

--- 09_c/Basics/08_File with c.c
+++ 09_c/Basics/08_File with c.c
@@ -2,10 +2,14 @@
 
 int main(){
     FILE *fPtr = fopen("cow.txt", "w");
-
     fprintf(fPtr, "Cow is awesome");
     fclose(fPtr);
 
 
+    FILE *fPtr2 = fopen("cow.txt", "r");
+    char line[100];
+
+    fgets(line,100, fPtr2);
+    printf()
     return 0;
 }
